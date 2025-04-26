62c6fdf75723fa87e8de9a2aa5f41e633c0723e8
Shree Nandan Das <nayan@Shrees-MacBook-Air.local>
2025-04-26 16:34:10 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+  const [isReset, setIsReset] = useState(false);
+
+  const startTimer = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      const id = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+      setIntervalId(id);
+    }
+  }
+  const stopTimer = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      clearInterval(intervalId);
+    }
+  }
+  const resetTimer = () => {
+    setIsReset(true);
+    setTime(0);
+    setIsRunning(false);
+    clearInterval(intervalId);
+  }
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }
+  const handleStart = () => {
+    startTimer();
+    setIsReset(false);
+  }
+  const handleStop = () => {
+    stopTimer();
+  }
+  const handleReset = () => {
+    resetTimer();
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Stopwatch</h1>
+        <div className="stopwatch">
+          <div className="time">{formatTime(time)}</div>
+          <div className="controls">
+            { isRunning ? (<button onClick={handleStop} >Stop</button>): <button onClick={handleStart}>Start</button>}
+            
+            
+            <button onClick={handleReset} disabled={isReset}>Reset</button>
+          </div>
+        </div>
       </header>
     </div>
   );
